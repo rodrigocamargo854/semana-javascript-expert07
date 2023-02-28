373bbb56c562c6b42c02f727a695e653d487f9dd
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2023-02-28 10:09:41 -0300

    resolved memory linked

--- classes/class01/pages/titles/src/workers/cardListWorker.js
+++ classes/class01/pages/titles/src/workers/cardListWorker.js
@@ -1,12 +1,14 @@
 onmessage = ({ data }) => {
   let counter = 0
 
-  console.log('activating blocking operation...', data.maxItems)
-  console.time('blocking-op')
-  // blocking function
-  // 1e5 = 100.000
-  for (; counter < data.maxItems; counter++) console.log('.')
-  console.timeEnd('blocking-op')
+  if (counter) {
+    console.log('activating blocking operation...')
+    console.time('blocking-op')
+    // blocking function
+    // 1e5 = 100.000
+    for (let counter = 0; counter < 1e5; counter++) console.log('.')
+    console.timeEnd('blocking-op')
+  }
 
   postMessage(
     { response: 'ok', data: counter }
