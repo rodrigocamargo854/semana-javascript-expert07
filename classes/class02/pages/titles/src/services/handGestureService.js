a5fe88997367aff1d3e0203b808478a3886374ea
rodrigocamargo <rodrigocamargo854@gmail.com>
2023-03-04 03:06:33 -0300

    hands detect ok

--- classes/class02/pages/titles/src/services/handGestureService.js
+++ classes/class02/pages/titles/src/services/handGestureService.js
@@ -1,12 +1,50 @@
+import { knownGestures,gestureStrings} from "../util/gestures.js"
+
+knownGestures
 export default class HandGestureService{
-    #finferpose
+    #gestureEstimator
     #handPoseDetection
     #handsVersion
     #detector = null
+
     constructor({fingerpose,handPoseDetection,handsVersion}){
-        this.#finferpose = fingerpose
+        this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures)
         this.#handPoseDetection = handPoseDetection
         this.#handsVersion = handsVersion
+    }
+
+    //recebe o array vindo do tensorflow
+    async estimate(keypoints3D){
+        const predictions = await this.#gestureEstimator.estimate(
+            this.#getLandMarksFromKeypoints(keypoints3D),
+            //porcentagem de confiança do gesto(90%)
+            9
+
+        )
+        return predictions.gestures
+    }
+
+    async * detectGestures(predictions){
+        for(const hand of predictions){
+            //caso não achar mãos continue
+            if(!hand.keypoints3D) continue
+
+            const gestures =  await this.estimate (hand.keypoints3D)
+            if(!gestures.length) continue;
+
+             const result = gestures.reduce((previous,current) =>(previous.score > current.score) ? previous:current)   
+            const {x,y} = hand.keypoints.find(keypoint => keypoint.name === 'index_finger_tip')
+            yield{ event: result.name,x,y} 
+
+            
+            console.log('detected', gestureStrings[result.name],x,y)
+            }
+    }
+
+    #getLandMarksFromKeypoints(keypoints3D){
+        return keypoints3D.map(keypoint =>
+            [keypoint.x,keypoint.y,keypoint.z]
+        )
     }
 
     async estimateHands(video){
--- classes/class02/pages/titles/src/views/handGestureView.js
+++ classes/class02/pages/titles/src/views/handGestureView.js
@@ -1,1 +1,13 @@
-export default class handGestureView{}
+export default class handGestureView{
+    loop(fn){
+        requestAnimationFrame(fn)
+    }
+
+    scrollPage(top){
+        scroll({
+            top,
+            behavior:"smooth"
+        })
+    }
+
+}
