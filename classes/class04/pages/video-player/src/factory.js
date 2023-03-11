a0a0f5e84364165497ed36a3485ff2571e060766
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2023-03-11 07:28:29 -0300

    video url fixed

--- classes/class04/pages/video-player/src/factory.js
+++ classes/class04/pages/video-player/src/factory.js
@@ -55,7 +55,8 @@ const factory = {
     return Controller.initialize({
       view: new View(),
       worker,
-      camera
+      camera,
+      videoUrl: `${rootPath}/assets/video.mp4`
     })
   }
 }
